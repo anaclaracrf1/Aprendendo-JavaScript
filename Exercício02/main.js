82e7dc1d00d9b934a5e126a5127028ffef9f6b2f
anas2 <ana05clara24@gmail.com>
2023-06-06 19:38:27 -0300

    colocando gênero e classificação

--- Exercício02/main.js
+++ Exercício02/main.js
@@ -2,9 +2,7 @@ var data = new Date()
 var ano = data.getFullYear()
 var ano_digitado = document.getElementById('ano_de_nascimento')
 var resultado = document.getElementById('resultado')
-
-
-
+var genero = document.querySelectorAll('.genero')
 
 function Verificar() {
 
@@ -13,8 +11,33 @@ function Verificar() {
     }else {
         
         var idade = ano - Number(ano_digitado.value)
-        resultado.innerHTML = `Você tem ${idade} anos`
-        
-    }
+        var genero_escolhido = ''
 
+        if (genero[0].checked) {
+            genero_escolhido = 'Homem'
+            if (idade >= 0 && idade <= 12){
+                resultado.innerHTML = `Você tem ${idade} anos e é um mini homem ;)`
+            }else if (idade >= 13 && idade <= 18){
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem jovem :)`
+            }else if (idade >= 19 && idade <= 65) {
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem adulto :]`
+            }else {
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem idoso :/`
+            }
+        }else if (genero[1].checked) {
+            genero_escolhido = 'Mulher'
+            if (idade >= 0 && idade <= 12){
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mini mulher ;)`
+            }else if (idade >= 13 && idade <= 18){
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher jovem :)`
+            }else if (idade >= 19 && idade <= 65) {
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher adulto :]`
+            }else {
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher idoso :/`
+            }
+        }
+    }
+    
+    
+    
 }
