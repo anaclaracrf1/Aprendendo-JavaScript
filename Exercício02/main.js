e348336a9d1b178aaba0952725dd19e45107986f
anas2 <ana05clara24@gmail.com>
2023-06-06 19:40:08 -0300

    Adicionando detalhes gênero e classificação

--- Exercício02/main.js
+++ Exercício02/main.js
@@ -3,6 +3,7 @@ var ano = data.getFullYear()
 var ano_digitado = document.getElementById('ano_de_nascimento')
 var resultado = document.getElementById('resultado')
 var genero = document.querySelectorAll('.genero')
+var emoji_feliz = document.createAttribute('img')
 
 function Verificar() {
 
@@ -16,24 +17,24 @@ function Verificar() {
         if (genero[0].checked) {
             genero_escolhido = 'Homem'
             if (idade >= 0 && idade <= 12){
-                resultado.innerHTML = `Você tem ${idade} anos e é um mini homem ;)`
+                resultado.innerHTML = `Você tem ${idade} anos e é um mini homem.`
             }else if (idade >= 13 && idade <= 18){
-                resultado.innerHTML = `Você tem ${idade} anos e é um homem jovem :)`
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem jovem.`
             }else if (idade >= 19 && idade <= 65) {
-                resultado.innerHTML = `Você tem ${idade} anos e é um homem adulto :]`
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem adulto.`
             }else {
-                resultado.innerHTML = `Você tem ${idade} anos e é um homem idoso :/`
+                resultado.innerHTML = `Você tem ${idade} anos e é um homem idoso.`
             }
         }else if (genero[1].checked) {
             genero_escolhido = 'Mulher'
             if (idade >= 0 && idade <= 12){
-                resultado.innerHTML = `Você tem ${idade} anos e é uma mini mulher ;)`
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mini mulher.`
             }else if (idade >= 13 && idade <= 18){
-                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher jovem :)`
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher jovem.`
             }else if (idade >= 19 && idade <= 65) {
-                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher adulto :]`
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher adulto.`
             }else {
-                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher idoso :/`
+                resultado.innerHTML = `Você tem ${idade} anos e é uma mulher idoso.`
             }
         }
     }
