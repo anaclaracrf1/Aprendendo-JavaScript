1028c8d7c06ecabb8f0ea6f9bc8e82dfd45504f2
anas2 <ana05clara24@gmail.com>
2023-06-12 20:04:36 -0300

    finalizando css e começando js

---
+++ Exercício03/main.js
@@ -0,0 +1,18 @@
+let número = document.getElementById('numero_desejado')
+let resultado = document.getElementById('resultado')
+var botão_calcular = document.getElementById('botão_calcular')
+
+// function Calcular() {
+//     if (número.value.length == 0){
+//         window.alert('Número inválido. Digite um número maior que 0')
+//     }else {
+//         let numero_convertido = Number(número.value)
+//         let contador = 1 
+//         for (contador = 1; contador == 10; contador++){
+//             var calculo = String(contador) + "x" numero_convertido.value "=" Number(contador*número);
+
+//             resultado.innerText = calculo
+            
+//         }
+//     }
+// }
