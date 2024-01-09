ad894b11299028f0a89a525c573ac5a45018751b
Ana Clara <ana05clara24@gmail.com>
2024-01-09 13:52:59 -0300

    adding a css and organizing the page

--- Exercício04/main.js
+++ Exercício04/main.js
@@ -1,1 +1,5 @@
- 
+ /* 1. event listener when click the button 
+ 2. random jogada do computador 
+ 3. checar quem ganhou a partir do click('jogador') e computador ('random')
+ 4. criar objeto com pontuação do jogador a cada click
+ 5. adicionar um botao reset para atualizar*/
